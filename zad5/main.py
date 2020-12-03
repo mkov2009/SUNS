fb9d16e9a5301a4069a6f200b0cce68bdd67ab2a
Maros Kovalak <kovalak.maros@gmail.com>
2020-12-03 16:02:10 +0100

    Added convolutional neural network

--- zad5/main.py
+++ zad5/main.py
@@ -1,71 +1,47 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
 import tensorflow as tf
 import pandas as pd
-from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-import pathlib
-import os
-
-
-def find_file_names(path):
-    list = pd.DataFrame(columns=['filenames'])
-
-    for root, directories, files in os.walk(path):
-        for name in files:
-            meno = name.split(".")[0]
-            list = list.append({'filenames': meno}, ignore_index=True)
-
-    return list
 
 
 if __name__ == '__main__':
+
     df = pd.read_csv("data/styles.csv")
-    df = df.sort_values(by=["id"])
-
-    data_dir = pathlib.Path("D:\\pythonProject\\zad5\\data\\images")
-
-    ids = find_file_names("data/images")
-
-    image_count = len(list(data_dir.glob('*.jpg')))
-    print(image_count)
-
-    df = df[~df.id.isin(ids)]
-
-    labels = df["gender"]
-    labels = labels.map({'Boys': 0, 'Girls': 1, 'Men': 2, 'Women': 3, 'Unisex': 4})
+    df["id"] = df.apply(lambda row: str(row["id"]) + ".jpg", axis=1)
 
     batch_size = 32
-    img_height = 60
-    img_width = 60
+    img_height = 32
+    img_width = 32
 
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
-        image_size=(img_height, img_width),
+    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1. / 255.,
+        validation_split=0.2
+    )
+
+    train_ds = image_generator.flow_from_dataframe(
+        dataframe=df,
+        directory="data/images",
+        x_col="id",
+        y_col="gender",
+        target_size=(img_height, img_width),
         batch_size=batch_size,
-        labels=labels)
+        subset="training"
+    )
 
-    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
+    val_ds = image_generator.flow_from_dataframe(
+        dataframe=df,
+        directory="data/images",
+        x_col="id",
+        y_col="gender",
+        target_size=(img_height, img_width),
         batch_size=batch_size,
-        labels=labels)
-
-    class_names = ["Boys", "Girls", "Men", "Women", "Unisex"]
+        subset="validation"
+    )
 
     num_classes = 5
 
     model = Sequential([
-        layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
         layers.Conv2D(16, 3, padding='same', activation='relu'),
         layers.MaxPooling2D(),
         layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -77,7 +53,7 @@ if __name__ == '__main__':
         layers.Dense(num_classes)
     ])
     model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  loss="categorical_crossentropy",
                   metrics=['accuracy'])
 
     epochs = 5
