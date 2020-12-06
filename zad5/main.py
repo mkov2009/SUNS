0074b32afd9a30f167c2e7c7a24fd41cbf235e4c
Maros Kovalak <kovalak.maros@gmail.com>
2020-12-06 15:19:36 +0100

    Convolutional

--- zad5/main.py
+++ zad5/main.py
@@ -1,8 +1,11 @@
+import datetime
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import pandas as pd
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+import tensorflow as tf
 
 
 if __name__ == '__main__':
@@ -10,18 +13,20 @@ if __name__ == '__main__':
     df = pd.read_csv("data/styles.csv")
     df["id"] = df.apply(lambda row: str(row["id"]) + ".jpg", axis=1)
 
-    batch_size = 32
-    img_height = 32
-    img_width = 32
+    batch_size = 50
+    img_height = 60
+    img_width = 60
+    epochs = 10
+    img_dir = "C:/images"
 
     image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1. / 255.,
+        rescale=1./255.,
         validation_split=0.2
     )
 
     train_ds = image_generator.flow_from_dataframe(
         dataframe=df,
-        directory="data/images",
+        directory=img_dir,
         x_col="id",
         y_col="gender",
         target_size=(img_height, img_width),
@@ -31,7 +36,7 @@ if __name__ == '__main__':
 
     val_ds = image_generator.flow_from_dataframe(
         dataframe=df,
-        directory="data/images",
+        directory=img_dir,
         x_col="id",
         y_col="gender",
         target_size=(img_height, img_width),
@@ -41,46 +46,43 @@ if __name__ == '__main__':
 
     num_classes = 5
 
-    model = Sequential([
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)
-    ])
-    model.compile(optimizer='adam',
-                  loss="categorical_crossentropy",
-                  metrics=['accuracy'])
+    model = Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(60, 60, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.25))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1024, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0001)))
+    model.add(layers.Dropout(0.25))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(num_classes, activation='softmax'))
 
-    epochs = 5
-    history = model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=epochs
-    )
+    print(model.summary())
 
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
+    log_dir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+    optimizer = tf.optimizers.Adam(learning_rate=0.1)
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
 
-    epochs_range = range(epochs)
+    training = model.fit(train_ds, epochs=epochs, validation_data=val_ds, callbacks=[tensorboard_callback])
 
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
+    # Summarize history for accuracy
+    plt.plot(training.history['accuracy'])
+    plt.plot(training.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
 
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
+    # Summarize history for loss
+    plt.plot(training.history['loss'])
+    plt.plot(training.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
     plt.show()
